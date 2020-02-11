77da48bb4f2ed35fcf2f28238b47ad09dee40099
Julio Pinto Coelho <juliopcrj@gmail.com>
2020-02-10 21:37:42 -0300

    First commit?

---
+++ build/CMakeFiles/Pongo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pongo.dir/src/main.cpp.o"
+  "Pongo.pdb"
+  "Pongo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pongo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
